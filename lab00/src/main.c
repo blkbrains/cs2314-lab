52fe548fbbc90ed4dfa07b6becb944ad0b6f7f9e
blkbrains <krismamwacha@gmail.com>
2026-01-21 12:34:49 -0600

    01/21

--- lab00/src/main.c
+++ lab00/src/main.c
@@ -4,9 +4,9 @@ int main(void) {
     printf("CS2314 Lab00 Starter Demo\n");
 
     // TODO: Replace the placeholder information below
-    printf("Name: YOUR_NAME_HERE\n");
-    printf("R#: ****XXXXX\n");
-    printf("Section: XXX\n");
+    printf("Name: Kristopher Mamwacha\n");
+    printf("R#: 11183884\n");
+    printf("Section: 002\n");
     printf("Message: I can use GitHub for CS2314 labs!\n");
 
     return 0;
