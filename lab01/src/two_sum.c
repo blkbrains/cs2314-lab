7db23e80afec0835f75b4904ac546faafb5f1eba
shawnli1995 <ligaoxiang1995@gmail.com>
2026-01-31 13:38:20 -0600

    lab01 updated2

--- lab01/src/two_sum.c
+++ lab01/src/two_sum.c
@@ -1,7 +1,6 @@
 #include "two_sum.h"
 
 int two_sum(const int* nums, int n, int target, int* out_i, int* out_j) {
-    if (!nums || !out_i || !out_j || n < 2) return 0;
 
     // TODO: implement Two Sum.
     // Requirements:
